42e222268493d017c313cf8695e75da97d6ae5be
lyy <yongred@nate.com>
2017-02-07 01:38:04 +0000

    add email send func

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -4,6 +4,18 @@ class EmailController < ApplicationController
 
 	def email_send
 		@email = params[:email]
+		@title = params[:title]
 		@content = params[:content]
+
+		gmail = Gmail.connect("yongred05@gmail.com","1111")
+		gmail.deliver do
+			to email
+			subject title
+			text_part do
+				body cotent
+			end
+		end
+
+		gmail.logout
 	end
 end
